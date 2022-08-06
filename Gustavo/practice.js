6a11f6fc9d2ec4746a8fb78287782daca955ab53
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-08-05 22:00:23 -0400

    add problems to solve

---
+++ Gustavo/practice.js
@@ -0,0 +1,20 @@
+// Sólo pueden codificar dentro de las llaves de la función
+
+// Problema 1.
+// Dado un array de enteros, retornar un nuevo array con el doble de cada valor. Por ejemplo:
+// [1, 2, 3] --> [2, 4, 6]
+
+const double = (x) => {};
+
+const doubleResult = double([4, 8, 5]);
+
+console.log(doubleResult);
+
+// Problema 2.
+// Dado un array de enteros, devolver un nuevo array con los numero pares.
+
+const evens = (x) => {};
+
+const evensResult = evens([5, 12, 13, 8, 49, 57]);
+
+console.log(evensResult);

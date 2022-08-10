98ee00885b1ee29aaab19185921231ab18fdd7a3
Adrian Mansilla <mansilladrian@gmail.com>
2022-08-09 20:48:59 -0400

    Ejercicios Adrian

---
+++ Adrian/practice.js
@@ -0,0 +1,28 @@
+// Sólo pueden codificar dentro de las llaves de la función
+
+// Problema 1.
+// Dado un entero, retornar un nuevo entero con el doble de valor. Por ejemplo:
+
+const double = (x) => {};
+
+const doubleResult = double(8);
+
+console.log(doubleResult);
+
+// Problema 2.
+// Dado un string en minúsculas, devolver el string en mayúsculas.
+
+const upperCase = (x) => {};
+
+const upperCaseResult = upperCase("let's code!");
+
+console.log(upperCaseResult);
+
+// Problema 3.
+// Dado un número tipo string, devlover éste número tipo number.
+
+const toNumber = (x) => {};
+
+const toNumberResult = upperCase('324');
+
+console.log(toNumberResult);
